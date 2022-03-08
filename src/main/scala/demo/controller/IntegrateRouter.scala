5263c6a1e4d91c41ea9e9ec94672d60fa34e437a
pigeon_gugugu <892197240@qq.com>
2022-03-08 23:39:55 +0800

    first version

--- src/main/scala/demo/controller/IntegrateRouter.scala
+++ src/main/scala/demo/controller/IntegrateRouter.scala
@@ -1,5 +1,6 @@
 package demo.controller
 
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import demo.controller.todo.{AddTodoItem, DeleteTodoItem, GetTodoItem, UpdateTodoItem}
@@ -11,7 +12,13 @@ object IntegrateRouter {
     router
   }
 
-  private val router: Route = pathPrefix("api") {
+  private val router: Route = respondWithDefaultHeaders(Seq(
+    RawHeader("Access-Control-Allow-Origin", "*"),
+    RawHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"),
+    RawHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With"),
+    RawHeader("Access-Control-Allow-Credentials", "true")
+  )
+  ){
     concat(
       pathPrefix("usr") {
         concat(
--- src/main/scala/demo/controller/todo/GetTodoItem.scala
+++ src/main/scala/demo/controller/todo/GetTodoItem.scala
@@ -21,41 +21,44 @@ object GetTodoItem {
               return complete(StatusCodes.Unauthorized, HttpEntity(ContentTypes.`application/json`,
                 ErrorResponse(StatusCode = "A003", body = Map("error" -> "IllegalToken")).toJson.toString
               ))
+            } else {
+              transaction {
+                val itemList = DataBase.todoItems.where(item => item.userID === userID).toList.map(x => Item(x.id, x.content, x.done, x.startAt.toString))
+                complete(StatusCodes.Accepted, HttpEntity(ContentTypes.`application/json`,
+                  TodoItemListResponse(
+                    body = Map("data" -> itemList)).toJson.toString
+                ))
+              }
             }
-            val itemList = transaction {
-              DataBase.todoItems.where(item => item.userID === userID)
-            }
-            complete(StatusCodes.Accepted, HttpEntity(ContentTypes.`application/json`,
-              TodoItemListResponse(
-                body = Map("data" -> itemList.toList.map(x => Item(x.id, x.content, x.done, x.startAt.toString)))).toJson.toString
-            ))
           }
         }
       },
       (get & path(LongNumber) & headerValueByName("Auth-Token")) { (id, token) =>
         val userID = checkJwtToken(token)
         if (userID == -1) {
-          return complete(StatusCodes.Unauthorized, HttpEntity(ContentTypes.`application/json`,
+          complete(StatusCodes.Unauthorized, HttpEntity(ContentTypes.`application/json`,
             ErrorResponse(StatusCode = "A003", body = Map("error" -> "IllegalToken")).toJson.toString
           ))
-        }
-        val itemOpt: Option[TodoItem] = transaction {
-          DataBase.todoItems.where(item => item.id === id).headOption
-        }
-        if (itemOpt.isEmpty) {
-          return complete(StatusCodes.NotFound, HttpEntity(ContentTypes.`application/json`,
-            ErrorResponse(StatusCode = "A003", body = Map("error" -> s"can't find TodoItem id = $id")).toJson.toString
-          ))
-        }
-        val item = itemOpt.get
-        if (item.userID != userID) {
-          complete(StatusCodes.Unauthorized, HttpEntity(ContentTypes.`application/json`,
-            ErrorResponse(StatusCode = "A004", body = Map("error" -> s"This Item is not belong to you!")).toJson.toString
-          ))
         } else {
-          complete(StatusCodes.Accepted, HttpEntity(ContentTypes.`application/json`,
-            SingleTodoItemResponse(body = Map("data" -> Item(item.id, item.content, item.done, item.startAt.toString))).toJson.toString
-          ))
+          val itemOpt: Option[TodoItem] = transaction {
+            DataBase.todoItems.where(item => item.id === id).headOption
+          }
+          if (itemOpt.isEmpty) {
+            complete(StatusCodes.Accepted, HttpEntity(ContentTypes.`application/json`,
+              ErrorResponse(StatusCode = "A003", body = Map("error" -> s"can't find TodoItem id = $id")).toJson.toString
+            ))
+          } else {
+            val item = itemOpt.get
+            if (item.userID != userID) {
+              complete(StatusCodes.Unauthorized, HttpEntity(ContentTypes.`application/json`,
+                ErrorResponse(StatusCode = "A004", body = Map("error" -> s"This Item is not belong to you!")).toJson.toString
+              ))
+            } else {
+              complete(StatusCodes.Accepted, HttpEntity(ContentTypes.`application/json`,
+                SingleTodoItemResponse(body = Map("data" -> Item(item.id, item.content, item.done, item.startAt.toString))).toJson.toString
+              ))
+            }
+          }
         }
       }
     )
